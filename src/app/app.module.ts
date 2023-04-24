549be8ecc24503018504372b27eda109894be829
MounicaPerepu <mounica.perepu@gmail.com>
2023-04-24 07:01:18 +0100

    Error Handling using HttpInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AuthService } from './services/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ErrorLoggerInterceptor } from './services/error-logger.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,9 +24,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
-  providers: [AuthService],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorLoggerInterceptor,
+      multi: true
+    },
+    AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/error-logger.interceptor.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+
+@Injectable()
+export class ErrorLoggerInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<any> {
+    return next.handle(request)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          const errorMessage = this.setError(error);
+          console.log(error);
+          throw error.error;
+        })
+      );
+  }
+
+  setError(error: HttpErrorResponse): string {
+    let errorMessage = 'Unknown error occured';
+    if(error.error instanceof ErrorEvent) {
+        // Client side error
+        errorMessage = error.error.message;
+    } else {
+        // server side error
+        if (error.status!==0) {
+            errorMessage = error.error.errorMessage;
+        }
+    }
+    return errorMessage;
+}
+}
